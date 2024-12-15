c798ad8e042615d30ef60301adf409a5421266f7
OHSOHYEON00 <ohsohyeon24@gmail.com>
2024-12-15 17:23:30 +0100

    fix: remove useless x-axis scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <DataProvider>
       <>
-        <div className="w-screen h-24 flex items-center px-8 md:px-20 bg-gray-100 border-box drop-shadow-sm border">
+        <div className="w-full h-24 flex items-center px-8 md:px-20 bg-gray-100 border-box drop-shadow-sm border">
           <span className="text-gray-700 font-bold text-4xl">Simulator</span>
         </div>
         <div className="bg-white font-sans h-screen text-gray-700 w-screen py-16 px-8 md:py-28 md:px-20 md:p-20 ">
